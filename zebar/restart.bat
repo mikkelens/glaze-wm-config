685c264bcb4ac9e440377ed48dabac96470f02a0
Mikkel <Mikkel123sej@gmail.com>
2024-10-11 02:19:45 +0200

    upgrade to zebar 2.0

--- zebar/restart.bat
+++ zebar/restart.bat
@@ -4,5 +4,6 @@ taskkill /im zebar.exe /f
 timeout /t 1
 @REM ping 127.0.0.1 -n 1 -w 500> nul
 powershell -WindowStyle hidden -Command ^
-  $monitors = zebar monitors; ^
-  foreach ($monitor in $monitors) { Start-Process -WindowStyle Hidden -FilePath \"zebar\" -ArgumentList \"open bar --args $monitor\" };
+  Start-Process -WindowStyle Hidden -FilePath \"zebar\"
+  @REM $monitors = zebar monitors; ^
+  @REM foreach ($monitor in $monitors) { Start-Process -WindowStyle Hidden -FilePath \"zebar\" -ArgumentList \"open bar --args $monitor\" };
--- zebar/start.bat
+++
@@ -1,5 +0,0 @@
-@REM Start hidden powershell script, which runs `zebar open bar --args ...` for every monitor.
-@echo off
-powershell -WindowStyle hidden -Command ^
-  $monitors = zebar monitors; ^
-  foreach ($monitor in $monitors) { Start-Process -WindowStyle Hidden -FilePath \"zebar\" -ArgumentList \"open bar --args $monitor\" };
